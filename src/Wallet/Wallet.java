c23657339322d8b045edd1219bb6b4c65050ab23
yuny0623 <overman522@gmail.com>
2022-09-26 15:13:54 +0900

    Add: Wallet logic added.

--- src/Wallet/Wallet.java
+++ src/Wallet/Wallet.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Wallet {
-    private List<String> keySaver = new ArrayList<>();
+    private List<EncKey> keySaver = new ArrayList<>();
 
+    public void saveKey(EncKey encKey){
+        this.keySaver.add(encKey);
+    }
+    public List<EncKey> getAllKey(){
+        if(keySaver.size() == 0) {
+            return this.keySaver; // 예외 처리 필요
+        }else{
+            return this.keySaver;
+        }
+    }
 }
