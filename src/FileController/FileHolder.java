a246e8f02ca8a0ca168cd3e5c1f0aa5572977924
yuny0623 <overman522@gmail.com>
2022-09-26 15:35:19 +0900

    Add: FileHolder stores Encrpyted File Text.

---
+++ src/FileController/FileHolder.java
@@ -0,0 +1,16 @@
+package FileController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHolder {
+    private static List<String> encFileList = new ArrayList<>();
+
+    public static List<String> getMyEncFileList(){
+        return encFileList;
+    }
+
+    public static void deleteMyEncFileList(){
+        encFileList.clear();
+    }
+}
