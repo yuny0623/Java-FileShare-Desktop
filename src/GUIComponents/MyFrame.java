db76bd20f627edf525b26e3190a1c385a3b3ee80
yuny0623 <overman522@gmail.com>
2022-09-27 19:56:25 +0900

    Add: Add Buttons in Frame.

--- src/GUIComponents/MyFrame.java
+++ src/GUIComponents/MyFrame.java
@@ -1,11 +1,57 @@
 package GUIComponents;
 
-import javax.swing.*;
+import FileController.AESKeyMaker;
+import Key.AsymmetricKeyGenerator;
 
-public class MyFrame extends JFrame {
+import javax.crypto.SecretKey;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+    JButton button1 = null;
+    JButton button2 = null;
+
+    JButton button3 = null;
+    JButton button4 = null;
+
     public MyFrame(){
         setTitle("ImageGhostClient");
-        setSize(300, 300);
+        setSize(300, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+
+
+        button1 = new JButton("Create Symmetric Key");
+        button2 = new JButton("Create ASymmetric Key");
+        button3 = new JButton("Send");
+        button4 = new JButton("Receive");
+
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
+        button4.addActionListener(this);
+
+        c.add(button1);
+        c.add(button2);
+        c.add(button3);
+        c.add(button4);
+
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == button1){
+            SecretKey secretKey = AESKeyMaker.generateAESKey();
+            System.out.println(secretKey.toString());
+        }else if(e.getSource() == button2){
+
+        }else if(e.getSource() == button3){
+
+        }
     }
 }
--- src/Key/MnemonicGenerator.java
+++ src/Key/MnemonicGenerator.java
@@ -5,6 +5,6 @@ public class MnemonicGenerator {
         Mnemonic Generator
      */
     public MnemonicGenerator(){
-        // 1000 size 로 매핑 
+        // 1000 size 로 매핑
     }
 }
