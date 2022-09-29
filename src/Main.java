278cd9eb4ba803802f952f227b56f47b108e71ae
yuny0623 <overman522@gmail.com>
2022-09-29 18:51:28 +0900

    Fix: code refactoring. getEncoded() of SecretKey is crashed while printing GUI. getEncoded() -> getEncoded().toString()

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,20 @@ import Server.Connection;
 
 import javax.crypto.SecretKey;
 import java.io.File;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         new MyFrame();
+
+        System.out.println("-----------------------");
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("ownerPublicKey", "18r1v38cb1o3ryg14o3ggo13ubgh34g13");
+        hashMap.put("ciphertext","-01bgbig1p398rg1viufb1kdv1");
+
+        Connection.postRequest("http://localhost:8080/test-post", hashMap);
     }
 }
 
--- src/Server/Connection.java
+++ src/Server/Connection.java
@@ -2,15 +2,13 @@ package Server;
 
 import ClientCustomException.NoServerException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
-
-
-
-
+import java.util.Iterator;
+import java.util.Set;
 
 public class Connection {
 
@@ -77,7 +75,7 @@ public class Connection {
     }
 
     /*
-        path variable 로 데이터 전달 
+        path variable 로 데이터 전달
      */
     public static HashMap<String, String> httpRequestPostWithPathVariable(String urlPath, String pathVariable){
         if(Connection.checkServerLive()){
@@ -86,4 +84,78 @@ public class Connection {
 
         return new HashMap<>();
     }
+
+    public static String postRequest(String pURL, HashMap < String, String > pList) {
+
+        String myResult = "";
+
+        try {
+            //   URL 설정하고 접속하기
+            URL url = new URL(pURL); // URL 설정
+
+            HttpURLConnection http = (HttpURLConnection) url.openConnection(); // 접속
+            //--------------------------
+            //   전송 모드 설정 - 기본적인 설정
+            //--------------------------
+            http.setDefaultUseCaches(false);
+            http.setDoInput(true); // 서버에서 읽기 모드 지정
+            http.setDoOutput(true); // 서버로 쓰기 모드 지정
+            http.setRequestMethod("POST"); // 전송 방식은 POST
+
+            http.setRequestProperty("content-type", "applicaiton/json;utf-8");
+            //--------------------------
+            // 헤더 세팅
+            //--------------------------
+            // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
+
+            //--------------------------
+            //   서버로 값 전송
+            //--------------------------
+            StringBuffer buffer = new StringBuffer();
+
+            // HashMap으로 전달받은 파라미터가 null이 아닌경우 버퍼에 넣어준다
+            if (pList != null) {
+
+                Set key = pList.keySet();
+
+                for (Iterator iterator = key.iterator(); iterator.hasNext();) {
+                    String keyName = (String) iterator.next();
+                    String valueName = pList.get(keyName);
+                    buffer.append(keyName).append("=").append(valueName);
+                }
+            }
+
+            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "UTF-8");
+            PrintWriter writer = new PrintWriter(outStream);
+            writer.write(buffer.toString());
+            writer.flush();
+
+
+            //--------------------------
+            //   Response Code
+            //--------------------------
+            //http.getResponseCode();
+
+
+            //--------------------------
+            //   서버에서 전송받기
+            //--------------------------
+            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "UTF-8");
+            BufferedReader reader = new BufferedReader(tmp);
+            StringBuilder builder = new StringBuilder();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                builder.append(str + "\n");
+            }
+            myResult = builder.toString();
+            return myResult;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return myResult;
+    }
+
 }
