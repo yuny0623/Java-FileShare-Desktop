02d107e4c3d0063a3f36fd5cd22eac81b875108a
yuny0623 <overman522@gmail.com>
2022-09-29 17:10:26 +0900

    Add: java configuration file added.

---
+++ src/Config.java
@@ -0,0 +1,8 @@
+/*
+    java 설정 파일
+ */
+
+public class Config {
+    private static final String IMAGE_STORE_PATH = "../Image";
+    private static final String SERVER_URL = "https://localhost:8080";
+}
