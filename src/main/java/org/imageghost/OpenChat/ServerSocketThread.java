beccfa3fc3ec020af9c7ed63860ab21ccab34c34
yuny0623 <overman522@gmail.com>
2022-11-13 17:40:47 +0900

    Feat: add encryption while sending text.

--- src/main/java/org/imageghost/OpenChat/ServerSocketThread.java
+++ src/main/java/org/imageghost/OpenChat/ServerSocketThread.java
@@ -1,7 +1,14 @@
 package org.imageghost.OpenChat;
 
+import javax.crypto.Cipher;
 import java.io.*;
 import java.net.Socket;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class ServerSocketThread extends Thread{
     Socket socket;
@@ -39,9 +46,21 @@ public class ServerSocketThread extends Thread{
 
             while(true){
                 String strIn = in.readLine();
-                server.broadCasting("[" + name + "]" + strIn);
+                if(strIn.charAt(0) == '@'){
+                    int tempIdx = strIn.indexOf(':');
+                    if(tempIdx == -1){
+                        server.broadCasting("[" + name + "]" + strIn);
+                    }else{
+                        String receiverName = strIn.substring(1, tempIdx);
+                        String message = strIn.substring(tempIdx, strIn.length());
+                        String receiverPublicKey = ChatServer.publicKeyList.get(receiverName);
+                        String encodedMessage = encode(message.getBytes(), receiverPublicKey);
+                        server.broadCasting("[" + name + "]" + encodedMessage);
+                    }
+                }else {
+                    server.broadCasting("[" + name + "]" + strIn);
+                }
             }
-
         }catch(IOException e){
             System.out.println(threadName + " 님이 퇴장했습니다.");
             server.removeClient(this);
@@ -52,5 +71,44 @@ public class ServerSocketThread extends Thread{
                 e.printStackTrace();
             }
         }
+    }
+
+    public String encode(byte[] plainData, String stringPublicKey) {
+        String encryptedData = null;
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] bytePublicKey = Base64.getDecoder().decode(stringPublicKey.getBytes());
+            X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(bytePublicKey);
+            PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+            byte[] byteEncryptedData = cipher.doFinal(plainData);
+            encryptedData = Base64.getEncoder().encodeToString(byteEncryptedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedData;
+    }
+
+    public byte[] decode(String encryptedData, String stringPrivateKey) {
+        byte[] byteDecryptedData = null;
+
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] bytePrivateKey = Base64.getDecoder().decode(stringPrivateKey.getBytes());
+            PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(bytePrivateKey);
+            PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+            byte[] byteEncryptedData = Base64.getDecoder().decode(encryptedData.getBytes());
+            byteDecryptedData = cipher.doFinal(byteEncryptedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return byteDecryptedData;
     }
 }
