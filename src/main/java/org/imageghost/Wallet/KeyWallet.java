45996743c31ec10d99612182f1347bf2e67a90f9
yuny0623 <overman522@gmail.com>
2022-11-13 20:53:10 +0900

    Refactor: KeyWallet method refactored.

--- src/main/java/org/imageghost/Wallet/KeyWallet.java
+++ src/main/java/org/imageghost/Wallet/KeyWallet.java
@@ -1,5 +1,6 @@
 package org.imageghost.Wallet;
 
+import org.imageghost.Key.KeyFactory;
 import org.imageghost.Key.Keys.ASymmetricKey;
 import org.imageghost.Key.Keys.SymmetricKey;
 
@@ -9,8 +10,8 @@ public class KeyWallet implements Wallet{
     private static HashMap<String, SymmetricKey> symmetricKeyMap = new HashMap<>();
     private static HashMap<String, ASymmetricKey> asymmetricKeyMap = new HashMap<>();
 
-    SymmetricKey mainSymmetricKey;
-    ASymmetricKey mainASymmetricKey;
+    private static SymmetricKey mainSymmetricKey;
+    private static ASymmetricKey mainASymmetricKey;
 
     private static int numberForSymmetricKey = 0;
     private static int numberForASymmetricKey = 0;
@@ -19,14 +20,17 @@ public class KeyWallet implements Wallet{
         symmetricKeyMap.put(String.valueOf(numberForSymmetricKey++), encKey);
     }
     public static void saveMainSymmetricKey(SymmetricKey encKey){
-        symmetricKeyMap.put("MainKey", encKey);
+        mainSymmetricKey = encKey;
     }
     public static HashMap<String, SymmetricKey> getAllSymmetricKey(){
         return symmetricKeyMap;
     }
     public static SymmetricKey getMainSymmetricKey(){
-        SymmetricKey findSymmetricKey =  symmetricKeyMap.get("MainKey");
-        return findSymmetricKey;
+        if(mainSymmetricKey == null){
+            SymmetricKey symmetricKey = KeyFactory.createSymmetricKey();
+            mainSymmetricKey = symmetricKey;
+        }
+        return mainSymmetricKey;
     }
     public static void deleteAllSymmetricKey(){
         symmetricKeyMap.clear();
@@ -37,7 +41,7 @@ public class KeyWallet implements Wallet{
         asymmetricKeyMap.put(String.valueOf(numberForASymmetricKey++), encKey);
     }
     public static void saveMainASymmetricKey(ASymmetricKey encKey){
-        asymmetricKeyMap.put("MainKey", encKey);
+        mainASymmetricKey = encKey;
     }
 
     public static HashMap<String, ASymmetricKey> getAllASymmetricKey(){
@@ -45,8 +49,11 @@ public class KeyWallet implements Wallet{
     }
 
     public static ASymmetricKey getMainASymmetricKey(){
-        ASymmetricKey findASymmetricKey =  asymmetricKeyMap.get("MainKey");
-        return findASymmetricKey;
+        if(mainASymmetricKey == null){
+            ASymmetricKey aSymmetricKey = KeyFactory.createAsymmetricKey();
+            mainASymmetricKey = aSymmetricKey;
+        }
+        return mainASymmetricKey;
     }
 
     public static void deleteAllASymmetricKey(){
