aec1591c3d6443652ac5b3403e1869a9c65b8050
yuny0623 <overman522@gmail.com>
2022-11-13 15:10:40 +0900

    Refactor: java desktop ui refactored.

--- src/main/java/org/imageghost/GUIComponents/FixedMainGui.java
+++ src/main/java/org/imageghost/GUIComponents/FixedMainGui.java
@@ -1,20 +1,46 @@
 package org.imageghost.GUIComponents;
 
+import org.imageghost.TcpTransport.ClientAction;
+import org.imageghost.TcpTransport.ServerAction;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FixedMainGui extends JFrame implements ActionListener {
-    
+
+    Container container = getContentPane();
+    JButton clientButton = new JButton("Client");
+    JButton serverButton = new JButton("Server");
+    JPanel buttonPanel = new JPanel();
+
     public FixedMainGui(){
         setTitle("ImageGhost");
         setSize(550, 400);
         setLocation(400, 400);
         setVisible(true);
+        container.setLayout(new BorderLayout());
+
+        buttonPanel.add("East", clientButton);
+        buttonPanel.add("West", serverButton);
+        container.add("South", buttonPanel);
+
+        clientButton.addActionListener(this);
+        serverButton.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource() == clientButton){
+            serverButton.setEnabled(false);
+            ClientAction clientAction = new ClientAction();
+            clientAction.action();
+        }
+        else if(e.getSource() == serverButton){
+            clientButton.setEnabled(false);
+            ServerAction serverAction = new ServerAction();
+            serverAction.action();
+        }
     }
 }
--- src/main/java/org/imageghost/TcpTransport/ClientAction.java
+++ src/main/java/org/imageghost/TcpTransport/ClientAction.java
@@ -16,6 +16,7 @@ public class ClientAction {
             String ipStr = ia.toString();
             // ia.toString() -> "DESKTOP-5QRSVM2/192.168.70.1"
             String ip = ipStr.substring(ipStr.indexOf("/") + 1);
+            System.out.println("1->>>>>>>>>>>>>>>>>>>>>>>>");
             new ClientGui(ip, Config.TCP_IP_CONNECTION_DEFAULT_PORT);
         }catch(UnknownHostException e){
             e.printStackTrace();
